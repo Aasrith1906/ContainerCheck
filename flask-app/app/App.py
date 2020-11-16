9944f624dbb026e77d2b5a4b48c595823a6cb380
Aasrith1906 <chennapragada.aasrith@gmail.com>
2020-11-17 00:16:20 +0530

    ingress working for rest api deployed on cc-website.api.com

--- flask-app/app/App.py
+++ flask-app/app/App.py
@@ -3,14 +3,11 @@ import requests
 
 app = Flask(__name__)
 
- 
-
-
 @app.route('/',methods=['GET'])
 def index():
-    r = requests.get("http://192.168.1.56:31351/Containers")
+    r = requests.get("http://cc-website.api.com/Containers")
     return  "<h1>oye shreeya you are cute</h1>"
 
 if __name__ == '__main__':
 
-    app.run(debug=True, host='0.0.0.0',port=5002)
+    app.run(host='0.0.0.0',port=5000)
